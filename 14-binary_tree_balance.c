185cee756219babf27fac59232e56d11df41ae24
DevPacho <frankjose2004@gmail.com>
2022-07-27 17:11:12 -0500

    Calculating the balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+* binary_tree_balance - function that measures the
+* balance factor of a binary tree.
+*
+* @tree: pointer to the root node of the tree to measure the balance factor.
+*
+* Return: the balance factor or if tree is NULL, return 0.
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	int calculate_balance;
+
+	calculate_balance = aux_binary_tree_height(tree->left) -
+		aux_binary_tree_height(tree->right);
+
+	return (calculate_balance);
+
+}
