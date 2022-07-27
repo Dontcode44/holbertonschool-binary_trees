79443782d2369b94473387b1600b794ac8a61ae2
DevPacho <frankjose2004@gmail.com>
2022-07-27 17:16:40 -0500

    [CHANGED] order of declarations

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,14 +12,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int calculate_balance;
+
 	if (!tree)
 		return (0);
-
-	int calculate_balance;
 
 	calculate_balance = aux_binary_tree_height(tree->left) -
 		aux_binary_tree_height(tree->right);
 
 	return (calculate_balance);
-
 }
