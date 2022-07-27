6e97e9cd703529aa41d7834198baddd4025d63c7
DevPacho <frankjose2004@gmail.com>
2022-07-27 00:24:04 -0500

    [FIXED] validation if parent exists

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_root - Function that checks if a given node is a root
- * @node: Pointer to the node to check
- *
- * Return: 1 || 0
- */
+* binary_tree_is_root - Function that checks if a given node is a root
+* @node: Pointer to the node to check
+*
+* Return: 1 || 0
+*/
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent)
 		return (0);
 
 	if (node->parent == NULL && node->left && node->right)
