59b28700f7f4e225d14ae7eed4b9f6b444d31f7e
DevPacho <frankjose2004@gmail.com>
2022-07-26 21:11:25 -0500

    Some minimun changes

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,13 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_size - Function that measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to find size
- * Return: Recursion
- */
+* binary_tree_size - Function that measures the size of a binary tree.
+*
+* @tree: Pointer to the root node of the tree to find size
+*
+* Return: binary tree size or NULL on failure.
+*/
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
