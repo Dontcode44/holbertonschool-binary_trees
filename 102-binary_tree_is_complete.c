f4d584cd75b9ab9892f764d5427f5c6b03a899e3
DevPacho <frankjose2004@gmail.com>
2022-07-28 01:55:00 -0500

    21. Is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include "10-binary_tree_depth.c"
+
+/**
+* binary_tree_is_complete - function that checks if a binary tree is complete.
+* 
+* @tree: pointer to the root node of the tree to check.
+*
+* Return: 1 || 0.
+*/
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	int left_depth, right_depth;
+
+	if (!tree)
+		return (0);
+
+	left_depth = binary_tree_depth(tree->left);
+	right_depth = binary_tree_depth(tree->right);
+
+	return (left_depth == right_depth && binary_tree_is_complete(tree->left) &&
+	binary_tree_is_complete(tree->right));
+}
