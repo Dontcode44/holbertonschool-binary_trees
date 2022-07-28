10cdc4a77c7d3577cebd4521ff5d33c72b50a663
DevPacho <frankjose2004@gmail.com>
2022-07-28 00:44:51 -0500

    [CHANGED] iterative way

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,43 +1,35 @@
 #include "binary_trees.h"
 
 /**
-* findLCA - aux function to finds the lowest common ancestor of two nodes.
-* 
+* binary_trees_ancestor - function that finds the lowest
+* common ancestor of two nodes.
+*
 * @first: pointer to the first node.
 * @second: pointer to the second node.
 *
-* Return: pointer to the lowest common ancestor node of the two given nodes or NULL.
-*/
-
-binary_tree_t *findLCA(const binary_tree_t *tree,
-const binary_tree_t *first, const binary_tree_t *second)
-{
-	if (!tree)
-		return (NULL);
-
-	if (tree->n > first && tree->n > second)
-		return findLCA(tree->left, first, second);
-
-	if (tree->n < first && tree->n < second)
-		return findLCA(tree->right, first, second);
-
-	return (tree);
-}
-
-/**
-* binary_trees_ancestor - function that finds the lowest common ancestor of two nodes.
-* 
-* @first: pointer to the first node.
-* @second: pointer to the second node.
-*
-* Return: pointer to the lowest common ancestor node of the two given nodes or NULL.
+* Return: pointer to the lowest common ancestor node of the
+* two given nodes or NULL.
 */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
+	binary_tree_t *cast = (binary_tree_t *)second;
+
 	if (!first || !second)
 		return (NULL);
 
-	return (findLCA(0, first, second) + 1);
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		first = first->parent;
+		second = cast;
+	}
+
+	return (NULL);
 }
