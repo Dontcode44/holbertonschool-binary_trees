b193ba7488bcb2073f5ae176a7b89d0fb359ff85
DevPacho <frankjose2004@gmail.com>
2022-07-28 20:52:24 -0500

    [FIXED] recursive way with auxs functions

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,5 +1,42 @@
 #include "binary_trees.h"
-#include "10-binary_tree_depth.c"
+
+/**
+* count_nodes - function that counts the nodes number in a binary tree.
+*
+* @tree: pointer to the root node of the tree to count the nodes number.
+*
+* Return: the number of nodes or 0 in failure.
+*/
+
+int count_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (count_nodes(tree->left) + count_nodes(tree->right) + 1);
+}
+
+/**
+* is_complete - aux function to checks if a binary tree is complete.
+*
+* @tree: pointer to the root node of the tree to check.
+* @index: index assigned to current node.
+* @nodes_number: number of nodes in the tree.
+*
+* Return: 1 if is a complete binary tree || 0 in failure.
+*/
+
+int is_complete(const binary_tree_t *tree, int index, int nodes_number)
+{
+	if (!tree)
+		return (1);
+
+	if (index >= nodes_number)
+		return (0);
+
+	return (is_complete(tree->left, 2 * index + 1, nodes_number) &&
+	is_complete(tree->right, 2 * index + 2, nodes_number));
+}
 
 /**
 * binary_tree_is_complete - function that checks if a binary tree is complete.
@@ -11,14 +48,8 @@
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int left_depth, right_depth;
-
 	if (!tree)
 		return (0);
 
-	left_depth = binary_tree_depth(tree->left);
-	right_depth = binary_tree_depth(tree->right);
-
-	return (left_depth == right_depth && binary_tree_is_complete(tree->left) &&
-	binary_tree_is_complete(tree->right));
+	return (is_complete(tree, 0, count_nodes(tree)));
 }
